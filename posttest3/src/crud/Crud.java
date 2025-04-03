bf8c894beb51855d8e5d1825fd1233d379cb073c
Ammar <142759310+AmmarNabilF@users.noreply.github.com>
2025-04-03 10:40:29 +0800

    Using inheritance for CRUD menu

---
+++ posttest3/src/crud/Crud.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+        
+
+/**
+ *
+ * @author ammar
+ */
+public class Crud<daftar> {
+    protected List<daftar> storage = new ArrayList<>();
+    
+    protected void tambah(daftar item){
+        storage.add(item);
+    }
+    
+    protected boolean hapus(daftar item){
+        if (storage.contains(item)){
+            storage.remove(item);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    protected void show(){
+        if (storage.isEmpty()){
+            System.out.println("\nItem kosong");
+        }
+        for (daftar item: storage){
+            System.out.println(item);
+        }
+    }
+    
+}
---
+++ posttest3/src/crud/CrudResep.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package crud;
+
+import model.Resep;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+/**
+ *
+ * @author ammar
+ */
+public class CrudResep extends Crud<Resep> {
+    
+    public void catatResep(Resep resep){
+        tambah(resep);
+    }
+        
+    public void showResep(){
+        show();
+    }
+//        if (storage.isEmpty()){
+//            System.out.println("\nTidak ada resep yang dibuat");
+//        }
+//        for (Resep r : storage){
+//            System.out.println("ID :" + r.getidResep() +
+//                    ", ID Produk: " + r.getidProduk() +
+//                    ", ID Bahan: " + r.getidBahan() +
+//                    ", Jumlah digunakan: " + r.getjumlahDigunakan() +
+//                    ", Satuan: " + r.getsatuan());
+//        }     
+//    }
+}
